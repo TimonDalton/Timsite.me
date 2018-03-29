90e8ac373691734bd77ceeab9d28b2560ef2de36
EC2 Default User <ec2-user@ip-172-31-18-82.us-east-2.compute.internal>
2018-03-29 13:53:28 +0000

    first styled homepage

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.use('/assets',express.static(__dirname+'/assets'));
 
 app.get("/", function(req,res){
+    
     res.render("home/home.ejs");
 });
 
